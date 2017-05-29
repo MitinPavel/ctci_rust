53dd3fd75bb80bb381314975f0bd3446938d8f94
Pavlo Mitin <mitin.pavel@gmail.com>
2017-05-29 09:11:31 +0100

    [01.01.03 URLify] Optimise
    Substitute `shifts` HashMap with a simple `usize` counter,
    so now the algorithm doesn't require O(N) extra space.

--- src/ch_01/urlify.rs
+++ src/ch_01/urlify.rs
@@ -1,43 +1,46 @@
 /// # URLify
 
-mod char_to_shift {
-    use std::collections::HashMap;
 
-    pub fn urlify(mut chars: &mut [char], len: usize) -> &[char] {
-        let mut shifts: HashMap<usize, usize> = HashMap::new();
-        let mut shift = 0;
+pub fn urlify(mut chars: &mut [char], true_length: usize) -> &[char] {
+    let mut space_count: usize = 0;
 
-        for i in 0..len {
-            shifts.insert(i, shift);
-            if chars[i] == ' ' {
-                shift += 2; // `%20` needs 2 extra places.
-            }
+    for i in 0..true_length {
+        if chars[i] == ' ' {
+            space_count += 1;
         }
-
-        for i in (0..len).rev() {
-            chars[i + shifts[&i]] = chars[i];
-            if chars[i] == ' ' {
-                chars[i] = '%';
-                chars[i + 1] = '2';
-                chars[i + 2] = '0';
-            }
-        }
-
-        chars
     }
+
+    let mut index = true_length + space_count * 2;
+
+    for i in (0..true_length).rev() {
+        if chars[i] == ' ' {
+            chars[index - 1] = '0';
+            chars[index - 2] = '2';
+            chars[index - 3] = '%';
+            index -= 3;
+        } else {
+            chars[index - 1] = chars[i];
+            index -= 1;
+        }
+    }
+
+    chars
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_checks_char_to_shift_01() {
+    fn it_checks_urlify() {
         let mut cs1: [char; 5] = ['a', ' ', 'b', ' ', ' '];
-        assert_eq!(&['a', '%', '2', '0', 'b'], super::char_to_shift::urlify(&mut cs1, 3));
+        assert_eq!(&['a', '%', '2', '0', 'b'], super::urlify(&mut cs1, 3));
 
         let mut cs2: [char; 2] = ['a', 'a'];
-        assert_eq!(&['a', 'a'], super::char_to_shift::urlify(&mut cs2, 2));
+        assert_eq!(&['a', 'a'], super::urlify(&mut cs2, 2));
 
         let mut cs3: [char; 4] = ['a', ' ', ' ', ' '];
-        assert_eq!(&['a', '%', '2', '0'], super::char_to_shift::urlify(&mut cs3, 2));
+        assert_eq!(&['a', '%', '2', '0'], super::urlify(&mut cs3, 2));
+
+        let mut cs4: [char; 4] = ['a', ' ', ' ', ' '];
+        assert_eq!(&['a', ' ', ' ', ' '], super::urlify(&mut cs4, 1));
     }
 }
