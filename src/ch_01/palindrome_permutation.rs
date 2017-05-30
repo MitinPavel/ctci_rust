0a5c3e6e350559cf752029f418324ead20de6d81
Pavlo Mitin <mitin.pavel@gmail.com>
2017-05-30 07:27:08 +0100

    [01.01.04 Palindrome Permutation] Optimise
    Memory consumption was reduced using an array of `bool`
    to count chars with odd count.

--- src/ch_01/palindrome_permutation.rs
+++ src/ch_01/palindrome_permutation.rs
@@ -4,6 +4,7 @@
 ///
 /// * ASCII symbols only
 /// * Case sensitive
+/// * Whitespaces are significant
 
 /// ## Using an array of char counts and finding is it more than one odd count
 mod char_counts {
@@ -32,10 +33,36 @@ mod char_counts {
     }
 }
 
+/// ## Optimizing space w/ a bool array
+mod bool_array {
+    const CHARSET_SIZE: usize = 128;
+
+    pub fn is_palindrome_permutation(string: String) -> bool {
+        let mut char_oddness: &mut [bool; CHARSET_SIZE] = &mut [false; CHARSET_SIZE];
+
+        for char in string.chars() {
+            char_oddness[char as usize] = !char_oddness[char as usize];
+        }
+
+        let mut odd_counts = 0;
+
+        for i in 0..CHARSET_SIZE {
+            if char_oddness[i] {
+                odd_counts += 1;
+            }
+            if odd_counts > 1 {
+                return false;
+            }
+        }
+
+        true
+    }
+}
+
 #[cfg(test)]
 mod test {
     #[test]
-    fn it_tests() {
+    fn it_tests_char_counts() {
         assert_eq!(true, super::char_counts::is_palindrome_permutation("".to_string()));
         assert_eq!(true, super::char_counts::is_palindrome_permutation("a".to_string()));
         assert_eq!(true, super::char_counts::is_palindrome_permutation("aa".to_string()));
@@ -91,5 +118,64 @@ mod test {
         assert_eq!(false, super::char_counts::is_palindrome_permutation("abc".to_string()));
         assert_eq!(false, super::char_counts::is_palindrome_permutation("abcd".to_string()));
         assert_eq!(false, super::char_counts::is_palindrome_permutation("aabbcdde".to_string()));
+    }
+
+    #[test]
+    fn it_tests_bool_array() {
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("a".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("aa".to_string()));
+
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("aab".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("aba".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("baa".to_string()));
+
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("baab".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("baba".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("bbaa".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("abab".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("abba".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("aabb".to_string()));
+
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("cbaab".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("cbaba".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("cbbaa".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("cabab".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("cabba".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("caabb".to_string()));
+
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("bcaab".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("bcaba".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("bcbaa".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("acbab".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("acbba".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("acabb".to_string()));
+
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("bacab".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("bacba".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("bbcaa".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("abcab".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("abcba".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("aacbb".to_string()));
+
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("baacb".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("babca".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("bbaca".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("abacb".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("abbca".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("aabcb".to_string()));
+
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("baabc".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("babac".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("bbaac".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("ababc".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("abbac".to_string()));
+        assert_eq!(true, super::bool_array::is_palindrome_permutation("aabbc".to_string()));
+
+        assert_eq!(false, super::bool_array::is_palindrome_permutation("ab".to_string()));
+        assert_eq!(false, super::bool_array::is_palindrome_permutation("ab".to_string()));
+        assert_eq!(false, super::bool_array::is_palindrome_permutation("abc".to_string()));
+        assert_eq!(false, super::bool_array::is_palindrome_permutation("abcd".to_string()));
+        assert_eq!(false, super::bool_array::is_palindrome_permutation("aabbcdde".to_string()));
     }
 }
