9b2e60d0dd62c531ccf8c5cd9b55e19b8e478a76
Pavlo Mitin <mitin.pavel@gmail.com>
2017-06-09 07:17:45 +0100

    [01.01.08 Zero Matrix] Implement space optimization
    The first row and first column optimization (the second solution
    from the book) was translated.

--- src/ch_01/zero_matrix.rs
+++ src/ch_01/zero_matrix.rs
@@ -1,56 +1,127 @@
 /// # 1.8 Zero Matrix
 
-pub fn zero_matrix(m: &mut Vec<Vec<u8>>) {
-    let mut untouched_cols: Vec<usize> = (0..m[0].len()).collect();
-    let mut untouched_rows: Vec<usize> = vec![];
-    let mut zero_rows: Vec<usize> = vec![];
+mod optimizing_col_iteration {
+    pub fn set_zeros(m: &mut Vec<Vec<u8>>) {
+        let mut untouched_cols: Vec<usize> = (0..m[0].len()).collect();
+        let mut untouched_rows: Vec<usize> = vec![];
+        let mut zero_rows: Vec<usize> = vec![];
 
-    for row in 0..m.len() {
-        let mut zero_row = false;
+        for row in 0..m.len() {
+            let mut zero_row = false;
 
-        untouched_cols.retain(|col| {
-            if m[row][*col] == 0 {
-                zero_row = true;
-                false
+            untouched_cols.retain(|col| {
+                if m[row][*col] == 0 {
+                    zero_row = true;
+                    false
+                } else {
+                    true
+                }
+            });
+
+            if zero_row {
+                zero_rows.push(row)
             } else {
-                true
+                untouched_rows.push(row)
             }
-        });
-
-        if zero_row {
-            zero_rows.push(row)
-        } else {
-            untouched_rows.push(row)
         }
-    }
 
-    for row in zero_rows {
-        //OPTIMIZE: Check if `m[row] = vec![0; m[row].len()]` performs better.
-        for col in 0..m[row].len() {
-            m[row][col] = 0;
-        }
-    }
-
-    for row in untouched_rows {
-        for col in 0..m[row].len() {
-            if !untouched_cols.contains(&col) {
+        for row in zero_rows {
+            //OPTIMIZE: Check if `m[row] = vec![0; m[row].len()]` performs better.
+            for col in 0..m[row].len() {
                 m[row][col] = 0;
             }
+        }
+
+        for row in untouched_rows {
+            for col in 0..m[row].len() {
+                if !untouched_cols.contains(&col) {
+                    m[row][col] = 0;
+                }
+            }
+        }
+    }
+}
+
+// The solution was rigorously translated
+// from the book second solution (as close as possible to the original pseudocode).
+mod first_col_and_row_optimization {
+    pub fn set_zeros(m: &mut Vec<Vec<u8>>) {
+        let mut first_row_has_zero = false;
+        let mut first_col_has_zero = false;
+
+        // Check if the first row has a zero.
+        for j in 0..m[0].len() {
+            if m[0][j] == 0 {
+                first_row_has_zero = true;
+                break
+            }
+        }
+
+        // Check if the first column has a zero
+        for i in 0..m.len() {
+            if m[i][0] == 0 {
+                first_col_has_zero = true;
+                break
+            }
+        }
+
+        // Check for zeros in the rest of the matrix
+        for i in 1..m.len() {
+            for j in 1..m[0].len() {
+                if m[i][j] == 0 {
+                    m[i][0] = 0;
+                    m[0][j] = 0;
+                }
+            }
+        }
+
+        // Nullify rows based on values in first column
+        for i in 1..m.len() {
+            if m[i][0] == 0 {
+                nullify_row(m, i)
+            }
+        }
+
+        // Nullify columns based on values in first row
+        for j in 1..m[0].len() {
+            if m[0][j] == 0 {
+                nullify_column(m, j);
+            }
+        }
+
+        // Nullify the first row
+        if first_row_has_zero {
+            nullify_row(m, 0)
+        }
+
+        // Nullify the first column
+        if first_col_has_zero {
+            nullify_column(m, 0);
+        }
+    }
+
+    fn nullify_row(matrix: &mut Vec<Vec<u8>>, row: usize) {
+        for j in 0..matrix[0].len() {
+            matrix[row][j] = 0
+        }
+    }
+
+    fn nullify_column(matrix: &mut Vec<Vec<u8>>, col: usize) {
+        for i in 0..matrix.len() {
+            matrix[i][col] = 0
         }
     }
 }
 
 #[cfg(test)]
 mod test {
-
-    #[test]
-    fn check_zero_matrix_3_3() {
+    fn check_set_zeros_3_3<F>(test: F) where F: Fn(&mut Vec<Vec<u8>>) -> () {
         let mut actual = vec![
             vec![1, 2, 3],
             vec![4, 5, 6],
             vec![7, 8, 9]
         ];
-        super::zero_matrix(&mut actual);
+        test(&mut actual);
         assert_eq!(vec![
             vec![1, 2, 3],
             vec![4, 5, 6],
@@ -62,7 +133,7 @@ mod test {
             vec![4, 5, 6],
             vec![0, 8, 9]
         ];
-        super::zero_matrix(&mut actual);
+        test(&mut actual);
         assert_eq!(vec![
             vec![0, 2, 3],
             vec![0, 5, 6],
@@ -74,7 +145,7 @@ mod test {
             vec![4, 0, 6],
             vec![7, 8, 9]
         ];
-        super::zero_matrix(&mut actual);
+        test(&mut actual);
         assert_eq!(vec![
             vec![1, 0, 3],
             vec![0, 0, 0],
@@ -86,7 +157,7 @@ mod test {
             vec![4, 5, 6],
             vec![7, 8, 9]
         ];
-        super::zero_matrix(&mut actual);
+        test(&mut actual);
         assert_eq!(vec![
             vec![0, 0, 0],
             vec![4, 5, 0],
@@ -98,7 +169,7 @@ mod test {
             vec![4, 5, 0],
             vec![7, 8, 9]
         ];
-        super::zero_matrix(&mut actual);
+        test(&mut actual);
         assert_eq!(vec![
             vec![1, 2, 0],
             vec![0, 0, 0],
@@ -106,34 +177,31 @@ mod test {
         ], actual);
     }
 
-    #[test]
-    fn check_zero_matrix_2_3() {
+    fn check_set_zeros_2_3<F>(test: F) where F: Fn(&mut Vec<Vec<u8>>) -> () {
         let mut actual = vec![
             vec![1, 2, 3],
             vec![4, 0, 6]
         ];
-        super::zero_matrix(&mut actual);
+        test(&mut actual);
         assert_eq!(vec![
             vec![1, 0, 3],
             vec![0, 0, 0]
         ], actual);
     }
 
-    #[test]
-    fn check_zero_matrix_2_2() {
+    fn check_set_zeros_2_2<F>(test: F) where F: Fn(&mut Vec<Vec<u8>>) -> () {
         let mut actual = vec![
             vec![1, 0],
             vec![1, 1]
         ];
-        super::zero_matrix(&mut actual);
+        test(&mut actual);
         assert_eq!(vec![
             vec![0, 0],
             vec![1, 0]
         ], actual)
     }
 
-    #[test]
-    fn check_zero_matrix_1_1_one() {
+    fn check_set_zeros_1_1_one<F>(test: F) where F: Fn(&mut Vec<Vec<u8>>) -> () {
         let mut actual = vec![
             vec![1]
         ];
@@ -142,8 +210,40 @@ mod test {
             vec![1]
         ];
 
-        super::zero_matrix(&mut actual);
+        test(&mut actual);
 
         assert_eq!(expected, actual)
+    }
+
+    fn check_set_zeros_1_1_zero<F>(test: F) where F: Fn(&mut Vec<Vec<u8>>) -> () {
+        let mut actual = vec![
+            vec![0]
+        ];
+
+        let expected = vec![
+            vec![0]
+        ];
+
+        test(&mut actual);
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn check_optimizing_col_iteration() {
+        check_set_zeros_3_3(super::optimizing_col_iteration::set_zeros);
+        check_set_zeros_2_3(super::optimizing_col_iteration::set_zeros);
+        check_set_zeros_2_2(super::optimizing_col_iteration::set_zeros);
+        check_set_zeros_1_1_zero(super::optimizing_col_iteration::set_zeros);
+        check_set_zeros_1_1_one(super::optimizing_col_iteration::set_zeros);
+    }
+
+    #[test]
+    fn check_first_col_and_row_optimization() {
+        check_set_zeros_3_3(super::first_col_and_row_optimization::set_zeros);
+        check_set_zeros_2_3(super::first_col_and_row_optimization::set_zeros);
+        check_set_zeros_2_2(super::first_col_and_row_optimization::set_zeros);
+        check_set_zeros_1_1_zero(super::first_col_and_row_optimization::set_zeros);
+        check_set_zeros_1_1_one(super::first_col_and_row_optimization::set_zeros);
     }
 }
