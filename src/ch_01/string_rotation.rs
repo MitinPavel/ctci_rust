3d5b94d41816a7b0d9ddd363ca97e9514cfbc2b5
Pavlo Mitin <mitin.pavel@gmail.com>
2017-06-15 07:07:03 +0100

    [01.01.09 String Rotation] Change in accordance with the book solution
    The book solution assumes an identical strings are rotations
    of each other. The rust solution was changed taking it into account.

--- src/ch_01/string_rotation.rs
+++ src/ch_01/string_rotation.rs
@@ -1,9 +1,9 @@
 /// # 1.9 String Rotation
 
 pub fn is_rotation(s1: &str, s2: &str) -> bool {
-    if s1 == s2 { return false }
-    if s1.len() < 2 { return false }
-    if s1.len() != s2.len() { return false }
+    if s1.len() == 0 || s1.len() != s2.len() {
+        return false
+    }
 
     let concatenation = format!("{}{}", s2, s2);
 
@@ -16,10 +16,10 @@ mod tests {
     fn check_is_rotation() {
         assert!(super::is_rotation("waterbottle", "erbottlewat"));
         assert!(super::is_rotation("xy", "yx"));
+        assert!(super::is_rotation("a", "a"));
+        assert!(super::is_rotation("blah", "blah"));
 
         assert!(!super::is_rotation("", ""));
-        assert!(!super::is_rotation("a", "a"));
-        assert!(!super::is_rotation("blah", "blah"));
         assert!(!super::is_rotation("blah", "hello"));
     }
 }
